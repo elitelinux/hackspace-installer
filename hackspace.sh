939680f75b06bd707a8608c1a016c04cbb9ef371
Juan Fernando Villa Hernández <elite.linux@gmail.com>
2014-05-31 22:46:32 -0500

    Update hackspace.sh

--- hackspace.sh
+++ hackspace.sh
@@ -1,3 +1,5 @@
 cd /var/www/
 git clone https://github.com/elitelinux/hack-space.git
 cd /var/www/hack-space/php
+mv $HOME/hackspace.conf /etc/apache2/conf-available/hackspace.conf
+sudo ln -s /etc/apache2/conf-available/hackspace.conf /etc/apache2/conf-enabled/hackspace.conf
